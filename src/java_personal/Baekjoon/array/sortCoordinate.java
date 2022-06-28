18349c5483a0fd2b8c392763b5e015a892e45e6c
HIMMEL <bbluesky7738@gmail.com>
2022-06-28 17:15:08 +0900

    first

---
+++ src/java_personal/Baekjoon/array/sortCoordinate.java
@@ -0,0 +1,26 @@
+package java_personal.Baekjoon.array;
+
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+
+public class sortCoordinate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < t; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			map.put(a, b);
+		}
+		
+		System.out.println(map);
+		sc.close();
+	}
+
+}
