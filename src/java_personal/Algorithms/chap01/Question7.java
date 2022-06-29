82557377351e00a82de3c2f87a1c47b03572a7bf
HIMMEL <bbluesky7738@gmail.com>
2022-06-29 16:35:12 +0900

    first

---
+++ src/java_personal/Algorithms/chap01/Question7.java
@@ -0,0 +1,17 @@
+package java_personal.Algorithms.chap01;
+
+public class Question7 {
+
+	public static void main(String[] args) {
+
+		int sum = 0;
+		int n = 7;
+		
+		for (int i = 1; i <= n; i++) {
+			sum += i;
+		}
+		
+		System.out.println("1 + 2 + 3 + 4 + 5 + 6 + 7 = " + sum);
+	}
+
+}
