0d4564406e22aaa8840c1fc9b367c34a710bdded
HIMMEL <bbluesky7738@gmail.com>
2022-07-03 08:36:41 +0900

    first

--- src/java_personal/Algorithms/chap01/Question12.java
+++ src/java_personal/Algorithms/chap01/Question12.java
@@ -4,16 +4,25 @@ public class Question12 {
 
 	public static void main(String[] args) {
 
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-				if (i == 0) {
-					System.out.print(i);
+		for (int i = 0; i <= 10; i++) {
+			if (i == 1) {
+				System.out.print("---+---------------------------");
+			}
+			else {
+				for (int j = 0; j < 10; j++) {
+					if (i == 0 && j == 0) {
+						System.out.print("   |");
+					}
+					else if (i == 0 && j != 0) {
+						System.out.printf("%3d", j);
+					}
+					else if (j == 0 && i != 0) {
+						System.out.printf("%2d |", i-1);
+					}
+					else
+						System.out.printf("%3d", (i-1)*j);
 				}
-				else if (j == 0 && i != 0) {
-					System.out.print(i);
-				}
-				else
-					System.out.printf("%3d", i*j);
+				
 			}
 			System.out.println();
 		}
