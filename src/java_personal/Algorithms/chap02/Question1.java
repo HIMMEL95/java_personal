78872e50706392ff6dd5a6c8eadddb1733067d51
HIMMEL <bbluesky7738@gmail.com>
2022-07-04 08:56:03 +0900

    first

--- src/java_personal/Algorithms/chap02/Question1.java
+++ src/java_personal/Algorithms/chap02/Question1.java
@@ -1,10 +1,34 @@
 package java_personal.Algorithms.chap02;
 
+import java.util.Random;
+
 public class Question1 {
+	
+	static int maxOf(int[] a) {
+		int max = a[0];
+		for (int i = 1; i < a.length; i++) {
+			if (a[i] > max)
+				max = a[i];
+		}
+		return max;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Random rand = new Random();
+		
+		int num = rand.nextInt(10);
+		int[] height = new int[num];
+		
+		System.out.println("키의 최댓값을 구합니다.");
+		System.out.println("사람 수 : " + num);
+		
+		System.out.println("키 값은 아래와 같습니다.");
+		for (int i = 0; i < num; i++) {
+			height[i] = 100 + rand.nextInt(90);
+			System.out.println("height[" + i + "] : " + height[i]);
+		}
 
+		System.out.println("최댓값은 " + maxOf(height) + " 입니다.");
 	}
 
 }
