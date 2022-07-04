2d6c711932f9a7577d1f1596955272fc5c933850
HIMMEL <bbluesky7738@gmail.com>
2022-07-04 09:16:51 +0900

    first

---
+++ src/java_personal/Algorithms/chap02/Question2.java
@@ -0,0 +1,15 @@
+package java_personal.Algorithms.chap02;
+
+public class Question2 {
+	
+	static void swap(int[] a, int idx, int idy) {
+		int t = a[idx]; 
+		a[idx] = a[idy]; 
+		a[idy] = t;
+	} 
+
+	public static void main(String[] args) {
+
+	}
+
+}
