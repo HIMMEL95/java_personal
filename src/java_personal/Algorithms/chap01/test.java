4ff4dd79c7a6c9ca0afb014c100be0a1179dea06
HIMMEL <bbluesky7738@gmail.com>
2022-07-05 21:00:10 +0900

    Revision

---
+++ src/java_personal/Algorithms/chap01/test.java
@@ -0,0 +1,11 @@
+package java_personal.Algorithms.chap01;
+
+/**
+ * test
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
