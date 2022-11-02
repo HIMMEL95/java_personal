f29dc813b5753e4f726ba72d1f663099f6de3e44
HIMMEL <bbluesky7738@gmail.com>
2022-11-02 09:02:00 +0900

    2022-11-02 09:01

--- src/java_personal/programmers/test.java
+++ src/java_personal/programmers/test.java
@@ -3,30 +3,21 @@ package java_personal.programmers;
 public class test {
 	
 	public static void main(String[] args) {
-		solution(13,17);
-		System.out.println("test : " + solution(13, 17));
+		System.out.println("test : " + solution(50000000000000l));
 	}
 	
-	 public static int solution(int left, int right) {
-	        int answer = 0;
-	        
-	        for (int i=left; i<=right; i++) {
-	        	int count = 0;
-	            for (int j = 1; j<=i; j++) {
-	                if (i % j == 0) {
-	                    count++;
-	                    System.out.println(count);
-	                }
-	            }
-	            if (count % 2 == 0) {
-	                answer += i;
-	                System.out.println(answer);
-	            } else {
-	                answer -= i;
-	                System.out.println(answer);
-	            }
-	        }
-	        return answer;
-	    }
-}
 
+    public static long solution(long n) {
+        long answer = 0;
+        
+        for (long i=0; i<n/2; i++) {
+            if (Math.pow(i, 2) == n) {
+                answer = (long)Math.pow(i+1, 2);
+                break;
+            } else {
+                answer = -1;
+            }
+        }
+        return answer;
+    }
+}
