737b32d59b6fbe46dec714d8dbbfc57abed2a1e3
HIMMEL <bbluesky7738@gmail.com>
2022-11-21 22:47:04 +0900

    asd

---
+++ src/java_personal/codeReview/Uniquecharacters.java
@@ -0,0 +1,12 @@
+package java_personal.codeReview;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Uniquecharacters {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> chars = new HashMap<>();
+
+        
+    }
+}
