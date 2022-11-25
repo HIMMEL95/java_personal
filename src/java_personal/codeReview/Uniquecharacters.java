2756347ed784559a27075cd6574ab7bf3a0f1ad2
HIMMEL <bbluesky7738@gmail.com>
2022-11-25 22:32:32 +0900

    2022-11-25 22:32

--- src/java_personal/codeReview/Uniquecharacters.java
+++ src/java_personal/codeReview/Uniquecharacters.java
@@ -5,8 +5,17 @@ import java.util.Map;
 
 public class Uniquecharacters {
     public int firstUniqChar(String s) {
-        Map<Character, Integer> chars = new HashMap<>();
+        Map<String, Integer> chars = new HashMap<String, Integer>();
 
+        String[] str = {};
+        int count = 0;
+
+        for (int i=0; i<s.length(); i++) {
+            if (!s.contains(s.charAt(i) + "0")) {
+            	str[i] = s.charAt(i) + "0";
+            }
+            System.out.println(str[i]);
+        }
         return 1;
     }
 }
