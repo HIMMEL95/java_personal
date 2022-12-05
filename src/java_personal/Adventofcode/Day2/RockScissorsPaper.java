d124a911d6f444335a7103d99ab708dcfc6363f8
HIMMEL <bbluesky7738@gmail.com>
2022-12-05 09:09:01 +0900

    2022-12-05 09:08

---
+++ src/java_personal/Adventofcode/Day2/RockScissorsPaper.java
@@ -0,0 +1,32 @@
+package java_personal.Adventofcode.Day2;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class RockScissorsPaper{
+	
+	private String A,X = "Rock";
+	private String B,Y = "Paper";
+	private String C,Z = "Scissors";
+	
+	public static void read() throws Exception {
+		BufferedReader reader = new BufferedReader(new FileReader("./src/java_personal/Adventofcode/Day2/input.txt"));
+		
+		
+		
+		String str;
+		int score = 0;
+		while ((str = reader.readLine()) != null) {
+			String[] game = str.split(" ");
+			System.out.println(game[0]);
+			System.out.println(game[1]);
+			System.out.println(str);
+		}
+		
+		reader.close();
+	}
+	
+	public static void main (String[] args) throws Exception {
+		read();
+	}
+}
