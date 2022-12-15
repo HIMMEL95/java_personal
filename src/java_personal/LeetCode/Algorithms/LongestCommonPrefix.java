98977484212ed0a7f2b2d8442df60e2cbd27bc8f
HIMMEL <bbluesky7738@gmail.com>
2022-12-15 22:46:53 +0900

    2022-12-15 22:46

--- src/java_personal/LeetCode/Algorithms/LongestCommonPrefix.java
+++ src/java_personal/LeetCode/Algorithms/LongestCommonPrefix.java
@@ -5,32 +5,42 @@ import java.util.List;
 
 public class LongestCommonPrefix {
     public static String Solution(String[] strs) {
-        String str = "";
         String result = "";
 
         if(strs.length == 1) {
             result = strs[0];
-        }
-
-        for (int i=1; i< strs.length; i++) {
-            for (int j=0; j<strs[i].length(); j++) {
-                if(strs[i-1].contains(Character.toString(strs[i].charAt(j)))) {
-                    result = "";
-                    result += strs[i].charAt(j);
+        } else {
+            for (int i=1; i< strs.length; i++) {
+                result="";
+                if (strs[i].length() == 1) {
+                    if (strs[i-1].contains(Character.toString(strs[i].charAt(0)))) {
+                        result += strs[i].charAt(0);
+                    }
+                } else {
+                    for (int j=1; j<=strs[i].length()-1; j++) {
+                        if(strs[i-1].contains(Character.toString(strs[i].charAt(j-1)))) {
+                            if (!strs[i-1].contains(Character.toString(strs[i].charAt(j)))) {
+                                result += strs[i-1].charAt(j-1);
+                                break;
+                            } else {
+                                result += strs[i-1].charAt(j-1);
+                            }
+                        }
+                    }
                 }
+                strs[i] = result;
             }
         }
+
         return result;
     }
 
     public static void main(String[] args) {
+        String[] arr3 = {"cir", "car"};
+        System.out.println(Solution(arr3));
         String[] arr = {"flower", "flow", "flight"};
         System.out.println(Solution(arr));
-        String[] arr1 = {"dog", "racecar", "car"};
+        String[] arr1 = {"ab", "a"};
         System.out.println(Solution(arr1));
-        String[] arr2 = {"a"};
-        System.out.println(Solution(arr2));
-        String[] arr3 = {"ab", "a"};
-        System.out.println(Solution(arr3));
     }
 }
