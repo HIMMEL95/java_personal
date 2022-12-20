6b3abe8ab4faf90c220dc43d5d38db2fbc7a5e5d
HIMMEL <bbluesky7738@gmail.com>
2022-12-20 22:41:49 +0900

    2022-12-20 22:41

--- src/java_personal/LeetCode/Algorithms/TopKFrequentElements.java
+++ src/java_personal/LeetCode/Algorithms/TopKFrequentElements.java
@@ -1,8 +1,21 @@
 package java_personal.LeetCode.Algorithms;
 
+import java.util.*;
+
 public class TopKFrequentElements {
     public static int[] topKFrequent(int[] nums, int k) {
         int[] result = new int[k];
+
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        for (int n : nums) {
+            hashMap.put(n, hashMap.getOrDefault(n, 0) + 1);
+        }
+        List<Integer> numbers = new ArrayList<>(hashMap.keySet());
+
+        Collections.sort(numbers, (value1, value2) -> hashMap.get(value2).compareTo(hashMap.get(value1)));
+        for (Integer a : numbers) {
+            System.out.println("key : " + a + ", value : " + hashMap.get(a));
+        }
 
         return result;
     }
@@ -11,6 +24,7 @@ public class TopKFrequentElements {
         int[] nums = {1, 1, 1, 2, 2, 3};
         int k = 2;
 
-        System.out.println(topKFrequent(nums, k));
+        topKFrequent(nums, k);
+//        System.out.println(topKFrequent(nums, k));
     }
 }
