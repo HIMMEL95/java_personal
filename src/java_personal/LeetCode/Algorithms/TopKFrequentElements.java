fa2e285cb87c9d65336585070e63f165844b3290
HIMMEL <bbluesky7738@gmail.com>
2022-12-20 21:31:05 +0900

    2022-12-20 19:31

---
+++ src/java_personal/LeetCode/Algorithms/TopKFrequentElements.java
@@ -0,0 +1,16 @@
+package java_personal.LeetCode.Algorithms;
+
+public class TopKFrequentElements {
+    public static int[] topKFrequent(int[] nums, int k) {
+        int[] result = new int[k];
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int k = 2;
+
+        System.out.println(topKFrequent(nums, k));
+    }
+}
