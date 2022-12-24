6fb5460a4fa7301fe77bbd602d67c0287892f6e5
HIMMEL <65998568+HIMMEL95@users.noreply.github.com>
2022-12-24 22:41:55 +0900

    Time: 2 ms (17.46%), Space: 42.5 MB (16.74%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,6 +1,24 @@
 class Solution {
     public String toLowerCase(String s) {
-        String result = s.toLowerCase();
+//         String result = s.toLowerCase();
+
+//         return result;
+        String result = "";
+
+        String[] c = new String[s.length()];
+
+        for (int i=0; i<s.length(); i++) {
+            c[i] = String.valueOf(s.charAt(i));
+        }
+
+        for (String a : c) {
+            if (Character.isUpperCase(a.charAt(0))) {
+                String str = a.toLowerCase();
+                result += str;
+            } else {
+                result += a;
+            }
+        }
 
         return result;
     }
