5f9efdfe8b410fba6620b9fd1fb5853a1c00ba21
HIMMEL <bbluesky7738@gmail.com>
2023-02-09 16:42:42 +0900

    2023-02-09 16:42

---
+++ src/java_personal/JavaCodingInterView/Chapter6/Abstract/Car.java
@@ -0,0 +1,9 @@
+package java_personal.JavaCodingInterView.Chapter6.Abstract;
+
+public interface Car {
+    public void speedUp();
+    public void slowDown();
+    public void turnRight();
+    public void turnLeft();
+    public String getCarType();
+}
---
+++ src/java_personal/JavaCodingInterView/Chapter6/Encapsulation/Cat.java
@@ -0,0 +1,7 @@
+package java_personal.JavaCodingInterView.Chapter6.Encapsulation;
+
+public class Cat {
+    private int mood = 50;
+    private int hungry = 50;
+    private int energy = 50;
+}
