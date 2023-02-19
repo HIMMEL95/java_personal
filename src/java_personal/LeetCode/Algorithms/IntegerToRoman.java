da07867e0baf2b42927f9b7569a4e02a040f5740
HIMMEL <bbluesky7738@gmail.com>
2023-02-20 08:08:27 +0900

    2023-02-20 08:08

---
+++ src/java_personal/LeetCode/Algorithms/IntegerToRoman.java
@@ -0,0 +1,20 @@
+package java_personal.LeetCode.Algorithms;
+
+import java.util.*;
+
+public class IntegerToRoman {
+
+    public static String intToRoman(int num) {
+        List<String> keys = new ArrayList<>(Arrays.asList("M", "D", "C", "L", "X", "V", "I"));
+        List<Integer> values = new ArrayList<>(Arrays.asList(1000, 500, 100, 50, 10, 5, 1));
+
+
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+        int num = 3;
+        System.out.println(intToRoman(num));
+    }
+}
